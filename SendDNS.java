7867bf56c527ed8b8fd12e24d36360ddd9053df7
JuanbiB <juanbiiberretta@gmail.com>
2017-11-12 18:59:36 -0500

    Making moves my boy.

--- SendDNS.java
+++ SendDNS.java
@@ -51,10 +51,28 @@ class SendDNS {
 		else if(args.length == 1){
 			nameserver = rootservers.get(0);
 			hostname = args[0];
-		} else {
+		}
+		/* E.g. java SendDNS -v <host-name> */
+		else if (args.length == 2) {
 			nameserver = args[0];
 			hostname = args[1];
 		}
+		/* java SendDNS -v -r <host-name>*/
+		else if (args.length == 3) {
+			
+		}
+		/* java SendDNS -v -n <name-sever> <host-name> */
+		else if (args.length == 4) {
+			
+		}
+		/* java SendDNS -r -v -n <name-server> <host-name>*/
+		else if (args.length == 5) {
+			
+		}
+		else {
+			System.out.println("Woah, too many args.\n");
+		}
+		
 		System.out.printf("Sending DNS Query (%s) to server %s\n",hostname,nameserver);
 	
 		byte[] serveraddr = null;
@@ -65,10 +83,67 @@ class SendDNS {
 		    System.out.printf("Invalid nameserver %s\n",nameserver);
 		    System.exit(1);
 		}
-	
+		
+		boolean found = false;
 		DNS result = sendRequest(serveraddr, hostname);
-			System.out.println("Response:\n");
-			System.out.println(result);
+		List<DNS.ResourceRecord> name_servers = new ArrayList<DNS.ResourceRecord>();	
+		List<DNS.ResourceRecord> alias_servers = new ArrayList<DNS.ResourceRecord>();
+		String found_ip = "";
+		while (true){
+			if (result != null) {
+				System.out.println("Response:\n");
+				System.out.println(result);
+				name_servers.clear();
+				alias_servers.clear();
+				for (DNS.ResourceRecord rec: result.rrlist) {
+					/* We found it, this is what we were looking for. All of our lives. */
+					if (rec.type == 1 && rec.name.equals(hostname) && rec.value != null) {
+						found_ip = rec.value;
+						found = true;
+					}
+					/* This is a name server we can query. */
+					else if (rec.type == 1 && rec.value != null) {
+						name_servers.add(rec);
+					}
+					/* This is an alias name for the host. */
+					else if (rec.type == 5 && rec.value != null) {
+						System.out.println("Found an alias for " + hostname + ": " + rec.value);
+						alias_servers.add(rec);
+						hostname = rec.value;
+					}
+				}
+				if (found) break;
+				/* There's no finding this chump. No aliases to refer to and no name server to hit up. */
+				if (name_servers.isEmpty() && alias_servers.isEmpty()) {
+					System.out.println("Not found... :(");
+					return;
+				}
+				serveraddr = null;
+				try {
+					/* If there's no aliases for this host. */
+					if (!alias_servers.isEmpty()) {
+						serveraddr = parseInetAddress(rootservers.get(0));
+					}
+					/* If there are name servers to ask, ask them. */
+					else {
+						serveraddr = parseInetAddress(name_servers.get(0).value);		
+					}
+				}
+				catch(NumberFormatException e){
+				    System.out.printf("Invalid nameserver %s\n",nameserver);
+				    System.exit(1);
+				}
+
+				result = sendRequest(serveraddr, hostname);
+			}
+			// If we timed out. TODO: Figure this out. Probably ask the next name server.
+			else {
+				return;
+			}
+		}
+		if (found) {
+			System.out.println(hostname + " resolved to: " + found_ip);
+		}
     }
 
     static DNS sendRequest(byte[] serveraddr, String hostname) throws IOException {
@@ -116,11 +191,25 @@ class SendDNS {
 	
 		// await the response
 		// TODO: Have it time out.
-		dSocket.receive(inPacket);
-	
-		byte[] answerbuf = inPacket.getData();
-	
-		DNS response = new DNS(answerbuf);
-		return response;
+		boolean time_out = false;
+		dSocket.setSoTimeout(100);
+		try {
+			dSocket.receive(inPacket);
+		}
+		catch (SocketTimeoutException e) {
+			time_out = true;
+		}
+		
+		if (time_out) {
+			// handle time out, go to next NS? 
+			System.out.println("timed out..");
+			return null;
+		}
+		else {
+			byte[] answerbuf = inPacket.getData();
+		
+			DNS response = new DNS(answerbuf);
+			return response;
+		}
     }
 }
